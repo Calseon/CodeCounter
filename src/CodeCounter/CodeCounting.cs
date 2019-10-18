5059f18bf42860777644876b159d0352bcf22d2b
CHUNG ZHENG <bczheng@edu.uwaterloo.ca>
2019-10-18 19:06:42 -0400

    finished initial prototype. all features work as intended. Should probably add multi-threading as a future improvement though. Takes quite a while to loop through all folders and subdirectories

---
+++ src/CodeCounter/CodeCounting.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace CodeCounter
+{
+    public enum CodingLanguage {
+        Csharp,
+        Cplus,
+        C,
+        Java,
+        Python,
+        VHDL,
+        MATLAB,
+        HTML
+    }
+
+    public class CodeCounting
+    {
+        private readonly Dictionary<CodingLanguage, bool> desiredLanguages;
+        private readonly Dictionary<CodingLanguage, long> numLines;
+        private readonly Dictionary<CodingLanguage, string> extensions = new Dictionary<CodingLanguage, string> {
+            {CodingLanguage.Csharp, ".cs" },
+            {CodingLanguage.Cplus, ".cpp" },
+            {CodingLanguage.C, ".c" },
+            {CodingLanguage.Java, ".java" },
+            {CodingLanguage.Python, ".py" },
+            {CodingLanguage.VHDL, ".vhd" },
+            {CodingLanguage.MATLAB, ".m" },
+            {CodingLanguage.HTML, ".html" },
+        };
+
+        public CodeCounting() {
+
+            // Initialize mappings of desired coding languages
+            // One for whether or not it is being checked (bool)
+            // One for counting the number of lines (long)
+            desiredLanguages = new Dictionary<CodingLanguage, bool>();
+            numLines = new Dictionary<CodingLanguage, long>();
+
+            var languages = Enum.GetValues(typeof(CodingLanguage));
+            foreach (CodingLanguage lang in languages) {
+                desiredLanguages.Add(lang,false);
+                numLines.Add(lang, 0);
+            }
+
+        }
+
+        private void ResetNumLines() {
+            var languages = Enum.GetValues(typeof(CodingLanguage));
+            foreach (CodingLanguage lang in languages)
+                numLines[lang] = 0;
+        }
+
+        public void SetLanguage(CodingLanguage lang, bool isSet) {
+            desiredLanguages[lang] = isSet;
+        }
+
+        public bool CountLines(string folder)
+        {
+            if (!Directory.Exists(folder))
+                return false;
+
+            ResetNumLines();
+
+            // Loop through each coding language
+            var languages = Enum.GetValues(typeof(CodingLanguage));
+            foreach (CodingLanguage lang in languages)
+            {
+                // Check if we want to count this language
+                if (!desiredLanguages[lang])
+                    continue;
+
+                // Get all the files matching our extension. This search INCLUDES sub-directories
+                var filepaths = Directory.GetFiles(folder, "*"+ extensions[lang], SearchOption.AllDirectories);
+
+                // Count the number of lines in each file, add it to our counter
+                foreach (var path in filepaths) {
+                    var lineCount = File.ReadLines(path).Count();
+                    numLines[lang] += lineCount;
+                }
+            }
+
+            return true;
+        }
+
+        public String GetLastResult() {
+            string output = "";
+            var languages = Enum.GetValues(typeof(CodingLanguage));
+            foreach (CodingLanguage lang in languages)
+            {
+                // Check if we want to count this language
+                if (!desiredLanguages[lang])
+                    continue;
+
+                output += lang.ToString("G") + ": " + numLines[lang] + "\n";
+            }
+            return output;
+        }
+    }
+}
--- src/CodeCounter/MainWindow.xaml.cs
+++ src/CodeCounter/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CodeCounter
 {
@@ -20,9 +8,72 @@ namespace CodeCounter
     /// </summary>
     public partial class MainWindow : Window
     {
+        CodeCounting counter;
+
         public MainWindow()
         {
             InitializeComponent();
+            counter = new CodeCounting();
+        }
+
+        private void BrowseFolder_Click(object sender, RoutedEventArgs e)
+        {
+            // Browse folder
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    tb_folder.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void CheckAll_Click(object sender, RoutedEventArgs e)
+        {
+            // Check all programming languages
+            cb_csharp.IsChecked = true;
+            cb_cplus.IsChecked = true;
+            cb_c.IsChecked = true;
+            cb_java.IsChecked = true;
+            cb_python.IsChecked = true;
+            cb_vhdl.IsChecked = true;
+            cb_matlab.IsChecked = true;
+            cb_html.IsChecked = true;
+        }
+
+        private void UncheckAll_Click(object sender, RoutedEventArgs e)
+        {
+            // Uncheck all programming languages
+            cb_csharp.IsChecked = false;
+            cb_cplus.IsChecked = false;
+            cb_c.IsChecked = false;
+            cb_java.IsChecked = false;
+            cb_python.IsChecked = false;
+            cb_vhdl.IsChecked = false;
+            cb_matlab.IsChecked = false;
+            cb_html.IsChecked = false;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            // Set the programming languages
+            counter.SetLanguage(CodingLanguage.Csharp,cb_csharp.IsChecked ?? false);// "?? false" returns false if the left side is null
+            counter.SetLanguage(CodingLanguage.Cplus, cb_cplus.IsChecked ?? false);
+            counter.SetLanguage(CodingLanguage.C, cb_c.IsChecked ?? false);
+            counter.SetLanguage(CodingLanguage.Java, cb_java.IsChecked ?? false);
+            counter.SetLanguage(CodingLanguage.Python, cb_python.IsChecked ?? false);
+            counter.SetLanguage(CodingLanguage.VHDL, cb_vhdl.IsChecked ?? false);
+            counter.SetLanguage(CodingLanguage.MATLAB, cb_matlab.IsChecked ?? false);
+            counter.SetLanguage(CodingLanguage.HTML, cb_html.IsChecked ?? false);
+
+            // Count the lines of code, returns true if completed successfully
+            if (counter.CountLines(tb_folder.Text)) {
+
+                // Output to textbox
+                tb_result.AppendText("-------------------"+ DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss") + "-------------------\n");
+                tb_result.AppendText("Root Folder: " + tb_folder.Text + "\n\n");
+                tb_result.AppendText(counter.GetLastResult());
+                tb_result.AppendText("\n");
+
+            }
         }
     }
 }
